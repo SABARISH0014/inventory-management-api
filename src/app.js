dce5c4a566cfcd4953b8c73519160005d5772ed8
Mey <meyappanrmn53953@gmail.com>
2026-04-11 15:10:54 +0530

    added supplier routes and updated sever.js, connected db

--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
 const productRoutes = require("./routes/productRoutes");
+const supplierRoutes = require("./routes/supplierRoutes");
+const stockRoutes = require("./routes/stockRoutes");
+const transactionRoutes = require("./routes/transactionRoutes");
 
+// Mount routes
 app.use("/api/products", productRoutes);
+app.use("/api/suppliers", supplierRoutes);
+app.use("/api/stock", stockRoutes);
+app.use("/api/transactions", transactionRoutes);
+
+// Test route
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
