0db6de4126558eb8878f2b88193e67039472b16e
Nithyashree C <mail4nithyashree@gmail.com>
2026-04-11 14:44:45 +0530

    Added report part

---
+++ src/services/reportService.js
@@ -0,0 +1,39 @@
+const Product = require("../models/Product");
+const Transaction = require("../models/Transaction");
+
+exports.getInventorySummary = async () => {
+  const products = await Product.find();
+
+  const totalProducts = products.length;
+
+  const totalQuantity = products.reduce(
+    (sum, p) => sum + p.quantity,
+    0
+  );
+
+  return {
+    totalProducts,
+    totalQuantity
+  };
+};
+
+exports.getLowStockProducts = async () => {
+  return await Product.find({
+    $expr: { $lt: ["$quantity", "$minStock"] }
+  });
+};
+
+exports.getTotalInventoryValue = async () => {
+  const products = await Product.find();
+
+  const totalValue = products.reduce(
+    (sum, p) => sum + p.price * p.quantity,
+    0
+  );
+
+  return { totalValue };
+};
+
+exports.getTransactionHistory = async () => {
+  return await Transaction.find().populate("productId");
+};
