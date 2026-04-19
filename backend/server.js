ddda373833bbf4b7bc3eae07e706ced6e4a534e9
SABARISH0014 <sabarish.0014@gmail.com>
2026-04-19 21:42:50 +0530

    deployment changes

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
+app.use(cors({
+  origin: '*' // Change this to your Vercel domain later for security
+}));
+
 // Start server
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "inventory-management-api-production-d93e.up.railway.app",
   headers: {
     "Content-Type": "application/json",
   },
