75932ab391f8986d1c8fbe83ca9f716a366f594f
SABARISH0014 <sabarish.0014@gmail.com>
2026-04-19 21:57:42 +0530

    deployment changes vercel

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "inventory-management-api-production-d93e.up.railway.app",
+  baseURL: "inventory-management-api-production-d93e.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
   },
